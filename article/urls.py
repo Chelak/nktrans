bb24931d805eaa5c80a9b7b73f041ca27927c739
scelac <celaserg@gmail.com>
2016-02-10 14:53:40 +0200

    add  views single article

--- article/urls.py
+++ article/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.article_list, name='article_list'),
-]
+                url(r'^$', views.article_list, name='article_list'),
+                url(r'^aricle/(?P<pk>[0-9]+)/$', views.article, name='article'),
+              ]
--- article/views.py
+++ article/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.utils import timezone
 
 from .models import Article
 # Create your views here.
+
+def article(request,pk):
+    single_article = get_object_or_404(Article, pk = pk)
+    return render(request,'single_article.html',{'article': single_article})
 
 
 def article_list(request):
